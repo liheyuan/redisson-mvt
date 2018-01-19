29518fa8c69660c8212011c34a96eba0b8fbdd47
coder4 <lihy@fenbi.com>
2018-01-19 18:54:22 +0800

    MOD: Init commit again

---
+++ src/main/java/RedissonMVTProperties.java
@@ -0,0 +1,46 @@
+/**
+ * @(#)RedissonMVTConfig.java, Jan 19, 2018.
+ * <p>
+ * Copyright 2018 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author coder4
+ */
+public class RedissonMVTProperties {
+
+    private Properties properties;
+
+    public RedissonMVTProperties() {
+
+    }
+
+    public boolean init() {
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            properties = new Properties();
+            properties.load(in);
+            in.close();
+            return true;
+        } catch (Exception e) {
+            System.err.println(e);
+            return false;
+        }
+    }
+
+    public String[] getSentinelList() {
+        return properties.getProperty("sentinelList").split(",");
+    }
+
+    public String getMasterName() {
+        return properties.getProperty("masterName");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("password");
+    }
+
+}
---
+++ src/main/java/TutorialApp.java
@@ -0,0 +1,46 @@
+/**
+ * @(#)TutorialApp.java, Jan 19, 2018.
+ * <p>
+ * Copyright 2018 fenbi.com. All rights reserved.
+ * FENBI.COM PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+import org.redisson.Redisson;
+import org.redisson.api.RSet;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+/**
+ * @author coder4
+ */
+public class TutorialApp {
+
+    public static void main(String[] args) {
+
+        RedissonMVTProperties prop = new RedissonMVTProperties();
+        if (!prop.init()) {
+            System.err.println("prop load failed");
+            System.exit(-1);
+        }
+
+        System.out.println(prop.getMasterName());
+
+        // use sentinel config
+        Config config = new Config();
+        config.useSentinelServers()
+                .setMasterName(prop.getMasterName())
+                .addSentinelAddress(prop.getSentinelList())
+                .setPassword(prop.getPassword());
+
+        // get redissonClient
+        RedissonClient redissonClient = Redisson.create(config);
+
+        RSet<Long> testSet = redissonClient.getSet("test1");
+        testSet.add(1l);
+        System.out.println(testSet.size());
+
+        // shutdown
+        redissonClient.shutdown();
+    }
+
+}
